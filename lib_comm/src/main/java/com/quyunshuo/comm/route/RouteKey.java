87ac24fbc57a56fcad828dbd5138feb94553ab03
Quyunshuo <quyunshuo@qq.com>
2020-04-12 18:34:33 +0800

    Modify: 添加ARouter统一管理类

---
+++ lib_comm/src/main/java/com/quyunshuo/comm/route/RouteKey.java
@@ -0,0 +1,11 @@
+package com.quyunshuo.comm.route;
+
+/**
+ * @Author: QuYunShuo
+ * @Time: 2020/4/12
+ * @Class: RouteKey
+ * @Remark: 阿里路由各种Key
+ */
+public interface RouteKey {
+
+}
---
+++ lib_comm/src/main/java/com/quyunshuo/comm/route/RouteUrl.java
@@ -0,0 +1,10 @@
+package com.quyunshuo.comm.route;
+
+/**
+ * @Author: QuYunShuo
+ * @Time: 2020/4/12
+ * @Class: RouteUrl
+ * @Remark: 阿里路由地址
+ */
+public interface RouteUrl {
+}
